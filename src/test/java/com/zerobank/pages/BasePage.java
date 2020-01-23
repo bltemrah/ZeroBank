5115d97ec236e2af18353fb3e425f9204aa1ed05
Your Name <bltemrah@gmail.com>
2020-01-23 20:13:04 +0000

    ZeroBank BasePage Locators

--- src/test/java/com/zerobank/pages/BasePage.java
+++ src/test/java/com/zerobank/pages/BasePage.java
@@ -1,4 +1,38 @@
 package com.zerobank.pages;
 
+import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage () {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy (css = "#account_summary_tab>a")
+    public WebElement acctSummary;
+
+    @FindBy (xpath = "//a[. ='Account Activity']")
+    public WebElement acctActivity;
+
+    @FindBy (xpath = "//a[. ='Transfer Funds']")
+    public WebElement transferFund;
+
+    @FindBy (xpath = "//a[. ='Pay Bills']")
+    public WebElement payBills;
+
+    @FindBy (xpath = "//a[. ='My Money Map']")
+    public WebElement myMoneyMap;
+
+    @FindBy (xpath = "//a[. ='Online Statements']")
+    public WebElement onlineStatements;
+
+    @FindBy (xpath = "(//a[@* = 'dropdown-toggle'])[1]")
+    public WebElement setting;
+
+    @FindBy (xpath = "(//a[@* = 'dropdown-toggle'])[2]")
+    public WebElement Myprofile;
+
 }
--- src/test/resources/features/AccountActivity.feature
+++ src/test/resources/features/AccountActivity.feature
@@ -2,8 +2,15 @@ Feature: Account activity
 
   Background:
     Given the user navigates to login page
-
-
-  Scenario: Account summary page
     When the user enter "username" "password"
+
+  @AcctActivity
+  Scenario: Account summary page
+
     Then the user should be able to see "Zero - Account Summary"
+
+  @AcctActvOption
+  Scenario: Account drop down options
+    And
+
+
